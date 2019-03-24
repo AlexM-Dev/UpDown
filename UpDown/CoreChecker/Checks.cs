bf366368f262e9875a46548081e50b9ea40fa5de
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2019-03-24 20:50:38 +1100

    Documentation.

--- UpDown/CoreChecker/Checks.cs
+++ UpDown/CoreChecker/Checks.cs
@@ -4,13 +4,47 @@ using System.Text;
 
 namespace UpDown.CoreChecker {
     public class Checks {
+        /// <summary>
+        /// Use a response code (>=400) to determine 
+        /// whether a website is down.
+        /// </summary>
         public bool ResponseCode { get; set; }
+
+        /// <summary>
+        /// A list of all allowed response codes, if Response Codes
+        /// are enabled.
+        /// </summary>
         public List<int> AllowedResponseCodes { get; set; }
+
+        /// <summary>
+        /// Use exception catch to determine if a site is down.
+        /// </summary>
         public bool TryCatch { get; set; }
+
+        /// <summary>
+        /// Use a set of keywords to determine if a site is down.
+        /// </summary>
         public bool Keyword { get; set; }
+
+        /// <summary>
+        /// Use Regex instead of a basic Contains() check.
+        /// </summary>
         public bool KeywordRegex { get; set; }
+
+        /// <summary>
+        /// Keywords to use for determining if a site is down.
+        /// </summary>
         public string[] Keywords { get; set; }
+
+        /// <summary>
+        /// Use Cloudflare for cloudflare-enabled sites.
+        /// </summary>
         public bool Cloudflare { get; set; }
+
+        /// <summary>
+        /// How many maximum retries to do when trying to solve
+        /// a CloudFlare challenge.
+        /// </summary>
         public int CfMaxRetries { get; set; }
 
         public Checks() {
--- UpDown/CoreChecker/Config.cs
+++ UpDown/CoreChecker/Config.cs
@@ -7,14 +7,49 @@ using UpDown.IO;
 
 namespace UpDown.CoreChecker {
     public class Config {
+        /// <summary>
+        /// The path to load addons from.
+        /// </summary>
         public string AddonPath { get; set; }
+
+        /// <summary>
+        /// The format of the addon names.
+        /// </summary>
         public string AddonsFormat { get; set; }
+
+        /// <summary>
+        /// Enable verbose logging.
+        /// </summary>
         public bool EnableLog { get; set; }
+
+        /// <summary>
+        /// Enable file logging.
+        /// </summary>
         public bool EnableLogFile { get; set; }
+
+        /// <summary>
+        /// The file to log to.
+        /// </summary>
         public string LogFilePath { get; set; }
+
+        /// <summary>
+        /// Whether the TOS has been accepted or not.
+        /// </summary>
         public bool AcceptedTOS { get; set; }
+
+        /// <summary>
+        /// The interval, in seconds, to check websites..
+        /// </summary>
         public int Interval { get; set; }
+
+        /// <summary>
+        /// The websites to check.
+        /// </summary>
         public List<string> Websites { get; set; }
+
+        /// <summary>
+        /// The base checks to perform for each website.
+        /// </summary>
         public Checks CheckSeries { get; set; }
 
         public Config() {
